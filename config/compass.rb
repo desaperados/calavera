0732d0fd388f97f715415307706275980b893026
David Currin <dave@2young2die.com>
2011-10-13 16:06:03 +1300

    Home page content, heroku sass config

--- config/compass.rb
+++ config/compass.rb
@@ -1,19 +1,23 @@
 # Complile stylesheets for production using:
 # compass compile --force
 
-if defined?(Sinatra) # Running within sinatra
-  project_path = Sinatra::Application.root
-  environment = :development
-else # command line tool
-  css_dir = File.join 'public', 'stylesheets'
-  relative_assets = true
-  environment = :production
+require 'sass/plugin/rack'
+
+use Sass::Plugin::Rack
+
+configure :production do
+  use Rack::Static,
+      urls: ['stylesheets'],
+      root: File.expand_path('../tmp'.__FILE__)
+
+  Sass::Plugin.options.merge!(template_location: 'views/stylesheets',
+                              css_location: 'tmp/stylesheets')
 end
 
-# Common Config
-sass_dir = File.join 'views', 'stylesheets'
-images_dir = File.join 'public', 'images'
-http_path = "/"
-http_images_path = "/images"
-http_stylesheets_path = "/stylesheets"
-output_style = :compressed
+configure :development do
+  sass_dir = File.join 'views', 'stylesheets'
+  images_dir = File.join 'public', 'images'
+  http_path = "/"
+  http_images_path = "/images"
+  http_stylesheets_path = "/stylesheets"
+end
